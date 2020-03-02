48cf49d675144e7de9b622a0a6d96f997496bb66
Jeffrey Lau <j8lau@ucsd.edu>
2020-03-02 02:14:50 -0800

    finished lab9

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -2,7 +2,7 @@
 
 // Call this function when the page loads (the "ready" event)
 $(document).ready(function() {
-	initializePage();
+    initializePage();
 })
 
 // Function that is called when the document is ready.
@@ -11,15 +11,17 @@ function initializePage() {
 }
 
 // Function that is called when the server responds to the AJAX call
-function onServerResponse(data){
+function onServerResponse(data) {
     var labElementId = getLabElementId(data.id);
+    labElementId = labElementId.replace("_", "-");
     var labElement = $(labElementId);
-    labElement.css({'background': 'yellow'});
+    console.log(labElement);
+    labElement.css("background", "yellow");
 }
 
 
 // Function to format the CSS selector
-function getLabElementId(id){
+function getLabElementId(id) {
     var labElementId = '#lab_' + id;
     return labElementId;
 }
